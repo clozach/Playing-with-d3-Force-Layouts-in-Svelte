4e4cdb679725186358d824cbcb4d6ed75a8a3e2e
Chris Lozac'h <clozach@gmail.com>
2019-09-03 23:02:14 -0700

    Added circle nodes to svg (no forces yet…just straight up d3 SVG shapes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,26 @@
 <script>
   import { onMount } from "svelte";
 
+  var nodes_data = [
+    { id: "Brimble", height: 0.1 },
+    { id: "Flakips", height: 0.3 },
+    { id: "Groppie", height: 0.9 },
+    { id: "Czostle", height: 0.5 },
+    { id: "Qualxir", height: 0.2 },
+    { id: "Merilio", height: 0.7 }
+  ];
+
   const setUpD3 = () => {
     var svg = d3.select("svg");
-    console.log(svg);
+    var node = svg
+      .append("g")
+      .attr("class", "nodes")
+      .selectAll("circle")
+      .data(nodes_data)
+      .enter()
+      .append("circle")
+      .attr("r", 50)
+      .attr("fill", "white");
   };
 
   onMount(setUpD3);
