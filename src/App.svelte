28d6a4a3e0048b1f3dcf6e76dc1c494781c8839d
Chris Lozac'h <clozach@gmail.com>
2019-09-03 22:03:32 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<style>
+ 
+.links line {
+  stroke: #999;
+  stroke-opacity: 0.6;
+}
+ 
+.nodes circle {
+  stroke: #fff;
+  stroke-width: 1.5px;
+}
+ 
+</style>
+<svg width="960" height="600"></svg>
+<script src="https://d3js.org/d3.v4.min.js"></script>
+<script>
+ 
+//create somewhere to put the force directed graph
+var svg = d3.select("svg"),
+ width = +svg.attr("width"),
+ height = +svg.attr("height");
+ 
+//d3 code goes here 
+ 
+</script>
