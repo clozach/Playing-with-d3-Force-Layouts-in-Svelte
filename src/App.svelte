67a4d29906669bd7a03a9a56fc41213681201e53
Chris Lozac'h <clozach@gmail.com>
2019-09-04 00:26:35 -0700

    🚀 Force Direction!!! 🤘
    Things to note:
    
    1. We've added a Simulation with the same data used to draw the original circles.
    
    2/ There's nothing special about our `nodes_data` array. The simulation thus far would work the same if we defined it as:
    
    ```
    var nodes_data = [ {},{},{},{},{},{},{} ]
    ```
    
    3. There's nothing special about the names given to the forces, either. They're just keys used for later lookup, if applicable. Don't believe me? Try swapping the `"charge_force"` and `"center_force"` labels!
    
    4. The forces are pretty straightforward, but the `on("tick"…` function, not so much, imho. Here's what I think I understand, currently.
    
      - Unlike with the strings passed to the `force` functions, "tick" label absolutely _does_ matter!
      - `d3.select("svg")` creates a _d3_ `Selection` object. (See [How Selections Work](https://bost.ocks.org/mike/selection/)), so the `svg` var isn't really a dom element, it's an object consisting of an array of groups, each being an array of elements (in this case, single elemtns), and some parent node data.
      - Attributes applied to a selection get propagated to every element within the selection, so `node.attr("cx", d => d.x)` looks at the _d3_ datum `d` associated with each element and assigns it as the `cx` value for the associated `svg` element in the DOM.
      - Using the <svelte:window> binding we can put the `forceCenter` force in the middle of the `svg` viewport.
    ```

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
   import { onMount } from "svelte";
 
+  let width;
+  let height;
+
   var nodes_data = [
     { id: "Brimble", height: 0.1 },
     { id: "Flakips", height: 0.3 },
@@ -21,6 +24,17 @@
       .append("circle")
       .attr("r", 50)
       .attr("fill", "white");
+
+    var simulation = d3.forceSimulation().nodes(nodes_data);
+    simulation
+      .force("charge_force", d3.forceManyBody())
+      .force("center_force", d3.forceCenter(width / 2, height / 2));
+    simulation.on("tick", () => {
+      //update circle positions to reflect node updates on each tick of the simulation
+      node
+        .attr("cx", d => d.x) // This comment is an annoying hack https://github.com/prettier/prettier-vscode/issues/352
+        .attr("cy", d => d.y);
+    });
   };
 
   onMount(setUpD3);
@@ -43,6 +57,7 @@
   }
 </style>
 
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 <svg />
 
 <h1>Hello</h1>
