7ea0544c954428aa372789b77b0a0b492bd3e80c
Chris Lozac'h <clozach@gmail.com>
2019-09-04 19:53:05 -0700

    Smaller balloons based on a new Figma export.
    Force changes:
    - weaker repulsion `strength` from `forceManyBody` for tighter spacing
    - smaller collision `radius` (tested w/positive fMB.strength so it should work once balloon groups start interacting)
    - tweaked `forceCenter`
    
    Also extracted the Balloon rendering chain into an exported function in the new `Balloon.svelte`. Note the use of `context="module"` in that file. This script prop allows us to import the function in App.svelte's `<script>` tag.
    
    This commit comes after hours of trying to get Svelte and d3 to behave nicely together. In particular, I was hoping to have the balloons be static HTML elements, imported by Svelte, and tagged with the appropriate node id's form the source data, after which point, hypothetically, d3 should take over and place them where epxected. Instead, all I get is elements that either won't render or won't reposition. 😩
    
    Why did I want to do that? Because it's tedious work, converting an exported SVG into a d3 function chain! Busy work. But that's ok. It's not worth gating the project to fix, I don't think, since it should be pretty easy to swap out if I ever find a solution.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import { balloonIt } from "./Balloon.svelte";
 
   let width;
   let height;
@@ -19,24 +20,15 @@
       .append("g")
       .attr("class", "nodes")
       .selectAll("unused") // Param detail seems irrelevant here, so far.
-      .data(nodes_data)
-      .enter()
-      .append("path")
-      .attr(
-        "d",
-        () =>
-          "M146.231 88C141.59475 149.06325 110.55825 179.34275 78.1905 180.40625C78.71225 180.728 78.98625 181.101 78.85975 181.46825C78.7125 181.8955 78.34075 182.20475 77.83725 182.4085C78.45875 182.785 78.98675 183.0765 79.29625 183.13525C81.0825 183.47525 82.3105 184.2075 82.18 185.1845C81.65975 189.07425 71.359 189.71475 69.8115 186.12075C69.3575 185.06575 70.63625 184.1365 72.54625 183.555C72.969 183.4265 72.994 182.79575 72.984 182.119C72.17475 181.7635 71.567 181.26075 71.41575 180.64475C71.376 180.48325 71.40175 180.33825 71.482 180.20975C39.4925 177.29075 7.890225 146.05275 0.7310525 88C-5.866625 34.5 33.30225 0 73.481 0C113.65975 0 150.0085 38.25 146.231 88z"
-      ) // Balloon Shape
-      .style("fill", "#C80E0E");
+      .data(nodes_data);
+
+    node = balloonIt(node);
 
     var simulation = d3.forceSimulation().nodes(nodes_data);
     simulation
-      .force("charge_force", d3.forceManyBody())
-      .force("collisions", d3.forceCollide().radius(d => 90))
-      .force("center_force", d3.forceCenter(width / 2 - 90, height / 2 - 90));
-    // 👆 Magic number, 90 _seems_ ever-so-slightly-more centered than
-    // what I tried on my first pass through: 73. 73 is the pixel width
-    // of the balloons un-scaled.
+      .force("charge_force", d3.forceManyBody().strength(-5))
+      .force("collisions", d3.forceCollide().radius(24))
+      .force("center_force", d3.forceCenter(width / 2 - 45, height / 2 - 45));
 
     simulation.on("tick", () => {
       //update circle positions to reflect node updates on each tick of the simulation
