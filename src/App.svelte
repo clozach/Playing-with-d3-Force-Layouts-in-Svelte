dc8321eb4b787254e927d1aafa7a78d274e6616e
Chris Lozac'h <clozach@gmail.com>
2019-09-21 12:40:33 -0700

    🚧 Start of isolating dependency-inverted functions
    My hope is that this will make it easier to follow the higher-level business logic.

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,12 @@
   import { onMount } from "svelte";
   import Field from "./Field.svelte";
   import {
+    levelMap,
+    drop,
+    balloonCreator,
+    runSim
+  } from "./FieldFunctions.svelte";
+  import {
     timeout,
     forceSimulation,
     forceX,
@@ -16,27 +22,6 @@
 
   var simulation; // Set after mount
   let datasource = initialBalloons;
-
-  // Remember, SVG origin is at the top left, so top is 0.1
-  const levelMap = {
-    top: 0.1,
-    up: 0.3,
-    middle: 0.5,
-    down: 0.7,
-    bottom: 0.9
-  };
-
-  const drop = balloons => {
-    const count = balloons._groups[0].length;
-    const totalDuration = 600; // In ms, per usual
-
-    balloons.each((d, i) => {
-      timeout(() => {
-        d.fx = null;
-        d.fy = null;
-      }, i * (totalDuration / count));
-    });
-  };
 
   const horizontalCenter = d => {
     return width / 2;
@@ -56,18 +41,6 @@
       .force("y", forceY().y(verticalLevelCenter));
   };
 
-  function balloonCreator() {
-    const templateBalloon = document.getElementsByClassName(
-      "template-balloon"
-    )[0];
-
-    // Inspired by https://stackoverflow.com/questions/18517376/d3-append-duplicates-of-a-selection @eagor
-    var clone = templateBalloon.cloneNode(true);
-    clone.getElementsByTagName("text")[0].innerHTML = this.__data__.height; // UGH
-    clone.classList.toggle("template-balloon");
-    return clone;
-  }
-
   function mergeNewData(selection, data) {
     return selection
       .data(data, d => d.id)
@@ -82,32 +55,6 @@
       );
   }
 
-  function runSim(sim, selection, data) {
-    // Without this, the simulation will have no more "energy" when
-    // balloons get added after the first run.
-    sim.alpha(1);
-
-    sim.nodes(data);
-
-    // This function works with `on("tick",…)` to ensure we only
-    // call `drop` on the very first tick.
-    var dropper = () => {
-      drop(selection);
-    };
-
-    sim.on("tick", () => {
-      //update balloon positions to reflect node updates on each tick of the simulation
-      selection.attr("x", d => d.x);
-      selection.attr("y", d => d.y);
-
-      if (dropper) {
-        dropper();
-        dropper = null;
-      }
-    });
-
-    sim.restart();
-  }
   const startSimulation = () => {
     if (!simulation) {
       simulation = setSimulationForces(forceSimulation());
---
+++ src/FieldFunctions.svelte
@@ -0,0 +1,79 @@
+<script context="module">
+  import { timeout } from "d3";
+
+  /**
+   * While it's a shame not to be able to write provably-pure functions,
+   * Elm-like, in Svelte, we can at least make it clear which functions
+   * depend exclusively on their arguments, versus those that pull this
+   * kind of shenanigan:
+   *
+   *     var componentVar;
+   *     function () { componentVar.modify() }
+   *
+   * How? By storing these closer-to-pure functions outside of the
+   * Svelte component that uses them.
+   *
+   * (What's the technical term for a function that _can_ modify its
+   * arguments…or, shudder, `this`, but can't access anything else not
+   * explicitly passed through the visible interface?)
+   */
+  // Remember, SVG origin is at the top left, so top is 0.1
+  export const levelMap = {
+    top: 0.1,
+    up: 0.3,
+    middle: 0.5,
+    down: 0.7,
+    bottom: 0.9
+  };
+
+  export const drop = balloons => {
+    const count = balloons._groups[0].length;
+    const totalDuration = 600; // In ms, per usual
+
+    balloons.each((d, i) => {
+      timeout(() => {
+        d.fx = null;
+        d.fy = null;
+      }, i * (totalDuration / count));
+    });
+  };
+
+  export function balloonCreator() {
+    const templateBalloon = document.getElementsByClassName(
+      "template-balloon"
+    )[0];
+
+    // Inspired by https://stackoverflow.com/questions/18517376/d3-append-duplicates-of-a-selection @eagor
+    var clone = templateBalloon.cloneNode(true);
+    clone.getElementsByTagName("text")[0].innerHTML = this.__data__.height; // UGH
+    clone.classList.toggle("template-balloon");
+    return clone;
+  }
+
+  export function runSim(sim, selection, data) {
+    // Without this, the simulation will have no more "energy" when
+    // balloons get added after the first run.
+    sim.alpha(1);
+
+    sim.nodes(data);
+
+    // This function works with `on("tick",…)` to ensure we only
+    // call `drop` on the very first tick.
+    var dropper = () => {
+      drop(selection);
+    };
+
+    sim.on("tick", () => {
+      //update balloon positions to reflect node updates on each tick of the simulation
+      selection.attr("x", d => d.x);
+      selection.attr("y", d => d.y);
+
+      if (dropper) {
+        dropper();
+        dropper = null;
+      }
+    });
+
+    sim.restart();
+  }
+</script>
