08d012cff0306d6b13ffd328d839660c85389541
Chris Lozac'h <clozach@gmail.com>
2019-09-22 01:32:39 -0700

    Added cloud generator with randomized heights & a tiny bit of horizontal flipping
    The result is surprisingly close to what I laid out by hand in Figma!
    
    Note: I did try playing around with balloon scaling, but the code started to get too crazy to track properly once I tried to close the gap betwen 1/5th-sized balloons. Perhaps another time…

--- src/ArrayHelpers.svelte
+++ src/ArrayHelpers.svelte
@@ -1,3 +1,8 @@
 <script context="module">
   export const pickOne = arr => arr[~~(Math.random() * arr.length)];
+
+  // https://stackoverflow.com/a/10050831/230615
+  export const integers = (lowEnd, highEnd) => {
+    return [...Array(highEnd - lowEnd + 1).keys()].map(i => i + lowEnd);
+  };
 </script>
--- src/Field.svelte
+++ src/Field.svelte
@@ -1,10 +1,32 @@
 <script>
+  import { scaleLinear } from "d3-scale";
+  import { integers } from "./ArrayHelpers.svelte";
   import Balloon from "./Balloon.svelte";
+  import Cloud from "./Cloud.svelte";
   import { gridlines } from "./FeatureToggles.svelte";
   import Gridlines from "./Gridlines.svelte";
 
-  export let width = 500;
-  export let height = 500;
+  export let width = 0;
+  export let height = 0;
+
+  let cloudWidth = 204; // Based on dimensions in Figma
+  let cloudHeight = 114;
+
+  // Adjust these until the clouds form a desirable pattern
+  let cloudMargin = cloudWidth ? 0.1 * cloudWidth : 0; // %ge negative margin on either side.
+  const cloudHeightMultiplier = 0.3; // 1 == zone twice the height of the clouds)
+
+  $: cloudZoneHeight = cloudHeight * cloudHeightMultiplier;
+  $: cloudCount = Math.ceil(width / cloudWidth) + 2; // Not sure why "+2, but it seems to work!"
+
+  const flipPercentageOfBalloons = perc => {
+    const flipHorizontal = cw => {
+      return `scale(-1,1) translate(-${cw},0)`;
+    };
+    return Math.random() > 0.2 ? flipHorizontal(cloudWidth) : "";
+  };
+
+  let scale = scaleLinear().range(-cloudMargin, width + cloudMargin);
 </script>
 
 <style>
@@ -21,6 +43,14 @@
 </style>
 
 <svg>
+  {#each integers(1, cloudCount) as i}
+    <Cloud
+      x={-cloudMargin + (i - 1) * (cloudWidth - 2 * cloudMargin)}
+      y={cloudZoneHeight * Math.random()}
+      width={cloudWidth}
+      height={cloudHeight}
+      transform={flipPercentageOfBalloons(0.2)} />
+  {/each}
   <g id="balloon-group" />
   <defs>
     <g id="empty-balloon">
