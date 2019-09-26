345bdb2fd6fdeb41e910e1a5fae4be78e8a44669
Chris Lozac'h <clozach@gmail.com>
2019-09-26 12:44:22 -0700

    Clouds now scale vertically 🎉
    Changes:
    - I'd declared a d3 linearScale, but never used it. Not sure why, but now it’s gone.
    - Clouds now scale in proportion to the window height.
    - `Balloon` 👉 `Cloud` in function name. 🙄

--- src/CloudLayer.svelte
+++ src/CloudLayer.svelte
@@ -1,41 +1,60 @@
 <script>
   import { integers } from "./ArrayHelpers.svelte";
-  import { scaleLinear } from "d3-scale";
   import Cloud from "./Cloud.svelte";
 
-  export let width;
-  export let height;
+  export let width = 0;
+  export let height = 0;
 
-  let cloudWidth = 204; // Based on dimensions in Figma
-  let cloudHeight = 114;
+  let cloudHeight = 0;
+  let cloudWidth = 0;
+  let cloudMargin = 0;
+
+  const rawWidth = 204; // Original cloud width in Figma
+  const rawHeight = 114; // Original cloud height in Figma
+  const cloudWidthOverCloudHeight = rawWidth / rawHeight;
+  const layerHeightOverViewHeight = 2 / 10;
+  const cloudHeightMultiplier = 1.3;
+
+  $: layerHeight = height * layerHeightOverViewHeight;
+  $: cloudHeight = layerHeight / cloudHeightMultiplier;
+  $: cloudWidth = cloudHeight * cloudWidthOverCloudHeight;
+  $: cloudScale = cloudHeight / rawHeight;
 
   // Adjust these until the clouds form a desirable pattern
-  let cloudMargin = cloudWidth ? 0.1 * cloudWidth : 0; // %ge negative margin on either side.
-  const cloudHeightMultiplier = 0.3; // 1 == zone twice the height of the clouds)
+  $: cloudMargin = cloudWidth ? 0.1 * cloudWidth : 0; // %ge negative margin on either side.
 
-  let scale = scaleLinear().range(-cloudMargin, width + cloudMargin);
-
-  $: cloudZoneHeight = cloudHeight * cloudHeightMultiplier;
-  $: cloudCount = Math.ceil(width / cloudWidth) + 2; // Not sure why "+2, but it seems to work!"
+  $: cloudCount = Math.ceil(width / cloudWidth);
 
   // Returns a horizontal flip transform `perc` percent of the time,
   // where `perc` is a value between 0 and 1.
   //
   // This is the simplest way I could think to keep the clouds from
   // looking too repetetive.
-  const flipPercentageOfBalloons = perc => {
+  const flipPercentageOfClouds = perc => {
     const flipHorizontal = cw => {
       return `scale(-1,1) translate(-${cw},0)`;
     };
     return Math.random() > 0.2 ? flipHorizontal(cloudWidth) : "";
   };
+
+  const x = (margin, idx, itemWidth) => {
+    return idx * (itemWidth - 2 * margin) - margin;
+  };
+
+  const y = (maxHeight, itemHeight) => {
+    return (maxHeight - itemHeight) * Math.random();
+  };
+
+  const transform = (percentToFlip, scale) => {
+    return `${flipPercentageOfClouds(percentToFlip)} scale(${scale})`;
+  };
 </script>
 
 <g id="cloud-group">
-  {#each integers(1, cloudCount) as i}
+  {#each integers(0, cloudCount) as i}
     <Cloud
-      x={-cloudMargin + (i - 1) * (cloudWidth - 2 * cloudMargin)}
-      y={cloudZoneHeight * Math.random()}
-      transform={flipPercentageOfBalloons(0.2)} />
+      x={x(cloudMargin, i, cloudWidth)}
+      y={y(layerHeight, cloudHeight)}
+      transform={transform(0.2, cloudScale)} />
   {/each}
 </g>
